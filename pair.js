28733c613ffae867bfe7d4497924a1f0c4f7ecef
lovemorenyeredzi94-hub <lovemorenyeredzi94@gmail.com>
2026-07-31 22:11:34 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -275,7 +275,7 @@ async function startpairing(kingbadboiNumber) {
         
         setTimeout(async () => {
             try {
-                let code = await bad.requestPairingCode(phoneNumber, 'NEXUSXMD'');
+                let code = await bad.requestPairingCode(phoneNumber, 'NEXUSXMD');
                 code = code?.match(/.{1,4}/g)?.join("-") || code;
                 
                 console.log(chalk.bgGreen.black(`📱 Pairing code for ${kingbadboiNumber}: ${chalk.white.bold(code)}`));
